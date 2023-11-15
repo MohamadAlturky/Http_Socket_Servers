ef1dbdb9f4f8d27ebb585ace55067a3355782c04
mohamad.alturky <mohamad.alturky@hiast.edu.sy>
2023-11-15 17:58:42 +0300

    fix status byg

--- client/src/SocketLoadTester.java
+++ client/src/SocketLoadTester.java
@@ -1,7 +1,5 @@
 package client;
 
-import stuff.Status;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -23,53 +21,54 @@ public class SocketLoadTester {
         SocketLoadTester.switchCase(url, port, threadCount, callCountPerThread, requestBody);
 
     }
+
     private static List<ApiResponseReport> reports = new ArrayList<>();
 
-    public static void switchCase(String url, int port, int threadCount, int callCountPerThread,String requestBody) {
+    public static void switchCase(String url, int port, int threadCount, int callCountPerThread, String requestBody) {
         Scanner scanner = new Scanner(System.in);
         boolean keepRunning = true;
         System.out.println("To run the test Press 1");
         System.out.println("To print the results of the tests Press 2");
         System.out.println("To close the application Press 0");
-        while (keepRunning){
+        while (keepRunning) {
             int code = scanner.nextInt();
-            if(code == 0){
+            if (code == 0) {
                 keepRunning = false;
             }
-            if (code == 1){
-                runLoadTest(url,port , threadCount,callCountPerThread,requestBody);
+            if (code == 1) {
+                runLoadTest(url, port, threadCount, callCountPerThread, requestBody);
             }
-            if(code == 2){
+            if (code == 2) {
                 double allTime = 0;
                 double minTime = 100000000;
                 double maxTime = 0;
                 int numberOfSuccessRequests = 0;
                 int numberOfFailureRequests = 0;
-                for (ApiResponseReport report :reports){
+                for (ApiResponseReport report : reports) {
                     allTime += report.executionTime;
-                    if(report.executionTime > maxTime){
+                    if (report.executionTime > maxTime) {
                         maxTime = report.executionTime;
                     }
-                    if (report.executionTime < minTime){
+                    if (report.executionTime < minTime) {
                         minTime = report.executionTime;
                     }
-                    if(report.status == Status.Success){
-                        numberOfSuccessRequests ++;
-                    }
-                    else{
-                        numberOfFailureRequests ++;
+                    if (report.status == Status.Success) {
+                        numberOfSuccessRequests++;
+                    } else {
+                        numberOfFailureRequests++;
                     }
                 }
-                double meanTime = allTime/reports.size();
-                System.out.println("mean time to response for all requests = "+ meanTime);
-                System.out.println("min time to response for all requests = "+ minTime);
-                System.out.println("max time to response for all requests = "+ maxTime);
-                System.out.println("number of failure response for all requests = "+ numberOfFailureRequests);
-                System.out.println("number of failure success for all requests = "+ numberOfSuccessRequests);
+                double meanTime = allTime / reports.size();
+                System.out.println("mean time to response for all requests = " + meanTime);
+                System.out.println("min time to response for all requests = " + minTime);
+                System.out.println("max time to response for all requests = " + maxTime);
+                System.out.println("number of failure response for all requests = " + numberOfFailureRequests);
+                System.out.println("number of failure success for all requests = " + numberOfSuccessRequests);
 
             }
         }
     }
+
     public static List<String> answers = new ArrayList<>();
 
     private static void runLoadTest(String url, int port, int threadCount, int callCountPerThread, String requestBody) {
@@ -84,7 +83,8 @@ public class SocketLoadTester {
                 PrintStream os = null;
                 try {
                     clientSocket = new Socket(url, port); // Create and connect the socket
-                    os = new PrintStream(clientSocket.getOutputStream()); // Create an output stream to send data to the server
+                    os = new PrintStream(clientSocket.getOutputStream()); // Create an output stream to send data to the
+                                                                          // server
                     is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
@@ -101,9 +101,9 @@ public class SocketLoadTester {
                         System.out.println(response);
                         is.readLine();
                         long end = System.currentTimeMillis();
-                        time = (end - start)/1000.0;
+                        time = (end - start) / 1000.0;
                         System.out.println("OK");
-                        reports.add(new ApiResponseReport(Status.Success,time));
+                        reports.add(new ApiResponseReport(Status.Success, time));
                         System.out.println("Client received: " + response);
                     }
                     os.close();
---
+++ client/src/Status.java
@@ -0,0 +1,6 @@
+package client;
+
+public enum Status {
+    Failure,
+    Success
+}
